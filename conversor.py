92e525a0ea76ab1c5a04a9e842b35e5678c186a3
GabrielJonasL <gabriel.j.lucio@hotmail.com>
2026-09-17 16:45:10 -0300

    feat: adicionado conversao de F para C na versao 1.0.0

--- conversor.py
+++ conversor.py
@@ -1,7 +1,8 @@
 def celsius_para_fahrenheit(celsius):
     return (celsius * 9/5) + 32
 
-
+def fahrenheit_para_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
 
 def exibir_menu():
     print("conversor de Temperatura")
