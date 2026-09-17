d70e6a7f3e02e2afa19b44813536052e2860da1f
GabrielJonasL <gabriel.j.lucio@hotmail.com>
2026-09-17 16:55:23 -0300

    fix: corrigido unidades de temperatura e adicionada opcao no menu de fahrenheit para celsius

--- conversor.py
+++ conversor.py
@@ -7,12 +7,13 @@ def fahrenheit_para_celsius(fahrenheit):
 def exibir_menu():
     print("conversor de Temperatura")
     print("1. Converter de Celsius para Fahrenheit")
-    print("2. Sair")
+    print("2. Converter de Fahrenheit para Celsius")
+    print("3. Sair")
 
     if __name__ == "__main__":
         while True:
                 exibir_menu()
-                opcao = input("Escolha a opcao (1-2): ")
+                opcao = input("Escolha a opcao (1-3): ")
 
                 if opcao == "1":
                     try:
@@ -21,7 +22,15 @@ def exibir_menu():
                         print(f"Resultado em Fahrenheit: {fahrenheit:.2f}F")
                     except ValueError:
                         print("Erro, digite novamente")
-                
+
+                elif opcao =="2":
+                    try:
+                        fahrenheit = float(input("Digite em Fahrenheit: "))
+                        celsius = fahrenheit_para_celsius(fahrenheit)
+                        print(f"Resultado em Celsius: {celsius:.2f}C")
+                    except ValueError:
+                        print("Erro, tente novamente.")
+
                 elif opcao == "3":
                     print("Encerramndo Conversor...")
                     break
